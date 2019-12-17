fb14a26a0e2c2ed881e2644010fca1df859f0ee6
chibitofu <erinmoony@gmail.com>
2019-12-16 19:44:30 -0800

    Fixed api key issue.

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var router = express.Router();
 var db = require('../models');
+var apiKey = process.env.APIKEY;
 
 router.get('/', function(req, res){
   res.render('index');
@@ -17,12 +18,12 @@ router.get('/favorites', function(req, res){
 router.get('/search', function(req, res){
   var query = req.query.q;
 
-  request('http://www.omdbapi.com/?s=' + query, function(err, response, body){
+  request('http://www.omdbapi.com/?s=' + query + "&apikey=" + apiKey, function(err, response, body){
     var json = JSON.parse(body);
     if(!err && json.Search){
       res.render('search', {movie: json.Search, q: query});
     } else {
-      request('http://www.omdbapi.com/?s=dragon+ball', function(err, response, body){
+      request('http://www.omdbapi.com/?s=dragon+ball' + "&apikey=" + apiKey, function(err, response, body){
       var notFound = JSON.parse(body);
       res.render('search', {movie: notFound.Search, q: query});
       });
@@ -32,7 +33,7 @@ router.get('/search', function(req, res){
 
 router.get('/movies/:id', function(req, res){
   var movieIndex = req.params.id;
-  request('http://www.omdbapi.com/?i=' + movieIndex + '&plot=full' + '&tomatoes=true', function(err, response, body){
+  request('http://www.omdbapi.com/?i=' + movieIndex + '&plot=full' + '&tomatoes=true' + "&apikey=" + apiKey, function(err, response, body){
     var json = JSON.parse(body);
     res.render('movies', {movie: json});
   });
@@ -40,7 +41,7 @@ router.get('/movies/:id', function(req, res){
 
 router.post('/favorites', function(req, res){
   var id = req.body.id;
-  request('http://www.omdbapi.com/?i=' + id, function(err, response, body){
+  request('http://www.omdbapi.com/?i=' + id + "&apikey=" + apiKey, function(err, response, body){
     var json = JSON.parse(body);
     var newFavorite = {
       imdbid: json.imdbID,
